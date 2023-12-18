573c87dee523eec592cd1fcf501b4953b1ec809b
Oli Hughes <olliehughes96@hotmail.co.uk>
2023-12-18 21:08:34 +0000

    Updated Cafe Stock to be more user friendly and simplified the code

--- cafe.py
+++ cafe.py
@@ -5,11 +5,6 @@ menu = ["Espresso", "Cortado", "Flat White", "Latte", "Cappuccino"]
 stock = {"Espresso": 13, "Cortado": 9, "Flat White": 23, "Latte": 25, "Cappuccino": 19}
 price = {"Espresso": 2.10, "Cortado": 2.50, "Flat White": 2.90, "Latte": 3.10, "Cappuccino": 3.10}
 
-# Code to print 'Current stock list' 
-def stock_list():
-    for key in stock.keys():
-        print(f"{key} - {stock[key]} - £{price[key]:.2f}")
-
 # Function to calculate amount of stock and how much value it has
 def total():
     total_stock = 0
@@ -18,6 +13,13 @@ def total():
         print(f"The total value for {items} is £{item_value:.2f}")
         total_stock += (stock[items] * price[items])
     print(f"The total value, of all the stock in the cafe, is £{total_stock:.2f}")
+
+# Code to print 'Current stock list' 
+def stock_list():
+    for key in stock.keys():
+        print(f"{key} x{stock[key]} - £{price[key]:.2f} each")
+
+
 
 print("-" * 40)
 print("\t Current Stock List \t")
@@ -32,18 +34,22 @@ while (not done):
 Would you like to:
 A - Add item
 B - Update stock
-C - Exit \n''')
+C - View total
+D - Exit \n''').capitalize()
 
     if choice == "A":
-        new_item = input("What item would you like to add: ").title()
+        new_item = input("What item would you like to add: \n").title()
         if new_item in menu:
-            print("*Sorry, that item is already on the menu*")
+            print("*Sorry, that item is already on the menu*", "\n", end="-"* 80)
             
 
         elif new_item not in menu:
             menu.append(new_item)
-            value = input("How many are currently in stock: ")
-            stock[new_item] = int(value)
+            quantity = input("How many are currently in stock: \n")
+            stock[new_item] = int(quantity)
+            value = input("How much does the item cost: \n£")
+            price[new_item] = float(value)
+
             print("-" * 40)
             print("\t Updated Stock List \t")
             print("-"* 40)
@@ -52,36 +58,20 @@ C - Exit \n''')
     # Start of code to update stock and find out what stock item needs updating
 
     elif choice == "B":
-        new_stock = input("What drink's stock level would you like to update: ")
+        new_stock = input("What drink's stock level would you like to update: ").title()
 
-        if new_stock.title() == "Espresso":
-            drink = input("How many drinks have been sold: ")
-            stock["Espresso"] -= int(drink)
+        if new_stock in stock:
+            drink = input("How many drinks have been sold: \n")
+            stock[new_stock] -= int(drink)
             for key in stock.keys():
                 print(key, "-", stock[key])
-            item_value = (stock["Espresso"] * price["Espresso"])
-            print(f"The total value for {menu[0]} is £{item_value:.2f}")
-
-        elif new_stock.title() == "Cortado":
-            drink = input("How many Cortado are there: ")
-            stock["Cortado"] = int(drink)
-            total()
-
-        elif new_stock.title() == "Flat White":
-            drink = input("How many Flat White are there: ")
-            stock["Flat White"] = int(drink)
-            total()
-
-        elif new_stock.title() == "Latte":
-            drink = input("How many Latte are there: ")
-            stock["Latte"] = int(drink)
-            total()
-        elif new_stock.title() == "Cappuccino":
-            drink = input("How many Cappuccino are there: ")
-            stock["Cappuccino"] = int(drink)
-            total()
-       
+            item_value = (stock[new_stock] * price[new_stock])
+            print(f"The total value for {new_stock} is now £{item_value:.2f}")
+        else:
+            print("*That drink isn't currently on the menu, please choose 'A' and add the new drink*", "\n" , end="-"* 80)
     elif choice == "C":
+        total()
+    elif choice == "D":
         done = True
     else:
         print("invalid")
